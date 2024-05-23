324f1ad2128e35331bcf91fe1421a35e404d3d37
victor@victor <victor.dmaina@gmail.com>
2024-05-23 23:38:18 +0300

    pass tests

--- lib/shoe.py
+++ lib/shoe.py
@@ -2,3 +2,25 @@
 
 class Shoe:
     pass
+
+
+class Shoe:
+    def __init__(self, brand, size):
+        self.brand = brand
+        self._size = size
+        self.condition = None
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            self._size = value
+        else:
+            print("size must be an integer")
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
